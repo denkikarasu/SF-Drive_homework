de4890e9183b38adaf75494acb276cb551fcb8ea
denkikarasu <denki.karasu@gmail.com>
2020-11-01 14:30:58 +0300

    Implement accordion with JS

---
+++ scripts/collapsible.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+
+  let collapsible = document.getElementsByClassName("collapsible");
+  let i;
+
+  for (i = 0; i < collapsible.length; i++) {
+    collapsible.item(i).addEventListener("click", function() {
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      content.classList.toggle("open");
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+};
