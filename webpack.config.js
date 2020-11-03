6ef92c892714ec3f2cf5846ef8efd6fdd4e11e06
denkikarasu <denki.karasu@gmail.com>
2020-11-03 17:09:20 +0300

    Add minimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
-    mode: 'production',
+    mode: 'development',
     output: {
         filename: 'main.js'
     },
-    plugins: [ new MiniCssExtractPlugin() ],
+    plugins: [ 
+        new MiniCssExtractPlugin(),
+        new TerserJSPlugin(), 
+        new OptimizeCSSAssetsPlugin() ],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserJSPlugin(), 
+            new OptimizeCSSAssetsPlugin()]
+    },
     module: {
         rules: [ 
             { 
