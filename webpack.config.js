33c1d96bc51ba5c605ac284ccfd985f87fea961e
denkikarasu <denki.karasu@gmail.com>
2020-11-03 16:10:45 +0300

    Link fonts separately
    Temporary solution to avoid issue with webpack, details in readme.md

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
-    mode: 'development',
+    mode: 'production',
     output: {
         filename: 'main.js'
     },
